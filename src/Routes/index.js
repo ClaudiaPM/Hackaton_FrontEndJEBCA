a6f458a4c71cd982aacad1d3c038d41f168fb889
Julio Pineda <jpinedatorres@hotmail.com>
2023-06-06 19:31:52 -0600

    Agregando nuevas pantallas para el front

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -6,6 +6,7 @@ router.get('/', (req, res) => res.render('index'))
 router.get('/Login', (req, res) => res.render('login'))
 router.get('/Register', (req, res) => res.render('register'))
 router.get('/Dashboard', (req, res) => res.render('dashboard'))
+router.get('/Perfil', (req, res) => res.render('perfil-usuario'))
 
 //URL not found
 router.get('/qwerty', (req,res)=>res.render('404'))
