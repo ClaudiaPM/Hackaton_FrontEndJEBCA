cf26942ab72dd5e2338b107c370715deb5420e08
Hardash01 <edwin98santos@gmail.com>
2023-06-08 18:48:05 -0600

    modificaciones del frontend

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -7,6 +7,9 @@ router.get('/Login', (req, res) => res.render('login'))
 router.get('/Register', (req, res) => res.render('register'))
 router.get('/Dashboard', (req, res) => res.render('dashboard'))
 router.get('/Perfil', (req, res) => res.render('perfil-usuario'))
+router.get('/listadousuarios', (req, res) => res.render('listadousuarios'))
+router.get('/perfilpaciente', (req, res) => res.render('perfilpaciente'))
+router.get('/creareventos', (req, res) => res.render('creareventos'))
 
 //URL not found
 router.get('/qwerty', (req,res)=>res.render('404'))
